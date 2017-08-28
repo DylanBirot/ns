dde6bd0aa291dc08389e0f4eff02302e68e0aef0
DylanBirot <dylanjbirot@gmail.com>
2017-08-28 21:01:29 +1000

    Design mobile completed!

--- design.js
+++ design.js
@@ -1,53 +1,52 @@
 $(document).ready(function() {
 
-        $("#scrolldown").click(function() { /*select class that triggers scroll*/
-            $('html, body').animate({
-                scrollTop: $("#ourprocess").offset().top - 100 /*class you want to scroll to!!*/
-            }, 1000); /*animation time length*/
-        });
-    
-    
-        $('.burger').click(function() {
-            
-                    if( !$('#burger').is(':checked')) {
-                        
-                        $( "#top" ).css('transform', 'rotate(45deg)').css('transition','all 0.4s ease-in-out');
-                        $( "#top" ).animate({
-                            marginTop: "0.5em",
-                          }, 200, function() {
-                            // Animation complete.
-                          });
-    
-                        $( "#bottom" ).css('transform', 'rotate(-45deg)').css('transition','all 0.4s ease-in-out');
-                        $( "#bottom" ).animate({
-                            marginTop: "-0.4em",
-                        }, 200, function() {
-                            // Animation complete.
-                        });
-      
-    
-                        $('.mobile-menu').css('visibility', 'visible');
-                        $('.mobile-menu').fadeIn();
-                        $('.mobile-menu a').css('animation', 'panup 1s ease-in-out');
-    
-                        
-    
-                    } else if ( $('#burger').is(':checked')) {
-    
-                        $( "#top" ).css('transform', '').css('margin-top', '');
-                        $( "#bottom" ).css('transform', '').css('margin-top', '');
-                        $('.mobile-menu a').css('animation', 'panout 1s ease-in-out');
-                        $('.mobile-menu').fadeOut();
-                        
-                        
-                    
-    
-                    }
-                    
-                });
-    
-    
-       
-    
-    });
-    
+	$("#scrolldown").click(function() { /*select class that triggers scroll*/
+		$('html, body').animate({
+			scrollTop: $("#ourprocess").offset().top - 100 /*class you want to scroll to!!*/
+		}, 1000); /*animation time length*/
+	});
+
+
+	$('.burger').click(function() {
+
+		if (!$('#burger').is(':checked')) {
+
+			$("#top").css('transform', 'rotate(45deg)').css('transition', 'all 0.4s ease-in-out');
+			$("#top").animate({
+				marginTop: "0.5em",
+			}, 200, function() {
+				// Animation complete.
+			});
+
+			$("#bottom").css('transform', 'rotate(-45deg)').css('transition', 'all 0.4s ease-in-out');
+			$("#bottom").animate({
+				marginTop: "-0.4em",
+			}, 200, function() {
+				// Animation complete.
+			});
+
+
+			$('.mobile-menu').css('visibility', 'visible');
+			$('.mobile-menu').fadeIn();
+			$('.mobile-menu a').css('animation', 'panup 1s ease-in-out');
+
+
+
+		} else if ($('#burger').is(':checked')) {
+
+			$("#top").css('transform', '').css('margin-top', '');
+			$("#bottom").css('transform', '').css('margin-top', '');
+			$('.mobile-menu a').css('animation', 'panout 1s ease-in-out');
+			$('.mobile-menu').fadeOut();
+
+
+
+
+		}
+
+	});
+
+
+
+
+});
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	var menu_active = false;
+
 
 	$("#scrolldown").click(function() { /*select class that triggers scroll*/
 		$('html, body').animate({
@@ -10,43 +10,43 @@ $(document).ready(function() {
 
 
 	$('.burger').click(function() {
-		
-				if( !$('#burger').is(':checked')) {
-					
-					$( "#top" ).css('transform', 'rotate(45deg)').css('transition','all 0.4s ease-in-out');
-					$( "#top" ).animate({
-						marginTop: "0.5em",
-					  }, 200, function() {
-						// Animation complete.
-					  });
 
-					$( "#bottom" ).css('transform', 'rotate(-45deg)').css('transition','all 0.4s ease-in-out');
-					$( "#bottom" ).animate({
-						marginTop: "-0.4em",
-					}, 200, function() {
-						// Animation complete.
-					});
-  
+		if (!$('#burger').is(':checked')) {
 
-					$('.mobile-menu').css('visibility', 'visible');
-					$('.mobile-menu').fadeIn();
-					$('.mobile-menu a').css('animation', 'panup 1s ease-in-out');
-
-					
-
-				} else if ( $('#burger').is(':checked')) {
-
-					$( "#top" ).css('transform', '').css('margin-top', '');
-					$( "#bottom" ).css('transform', '').css('margin-top', '');
-					$('.mobile-menu a').css('animation', 'panout 1s ease-in-out');
-					$('.mobile-menu').fadeOut();
-					
-					
-				
-
-				}
-				
+			$("#top").css('transform', 'rotate(45deg)').css('transition', 'all 0.4s ease-in-out');
+			$("#top").animate({
+				marginTop: "0.5em",
+			}, 200, function() {
+				// Animation complete.
 			});
+
+			$("#bottom").css('transform', 'rotate(-45deg)').css('transition', 'all 0.4s ease-in-out');
+			$("#bottom").animate({
+				marginTop: "-0.4em",
+			}, 200, function() {
+				// Animation complete.
+			});
+
+
+			$('.mobile-menu').css('visibility', 'visible');
+			$('.mobile-menu').fadeIn();
+			$('.mobile-menu a').css('animation', 'panup 1s ease-in-out');
+
+
+
+		} else if ($('#burger').is(':checked')) {
+
+			$("#top").css('transform', '').css('margin-top', '');
+			$("#bottom").css('transform', '').css('margin-top', '');
+			$('.mobile-menu a').css('animation', 'panout 1s ease-in-out');
+			$('.mobile-menu').fadeOut();
+
+
+
+
+		}
+
+	});
 
 
 	/*var prevScroll = 0;
@@ -74,7 +74,7 @@ $(document).ready(function() {
 			}
 		}
 
-		prevScroll = scrollTop 
+		prevScroll = scrollTop
 
 	}); */
 
